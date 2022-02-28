0c01a1b460ee627ccf559bb2b1e4d021d8caf23e
syedibrahim98 <masyedibrahim449@gmail.com>
2022-02-28 11:14:42 +0530

    MyTask

---
+++ src/org/test/GitSample.java
@@ -0,0 +1,33 @@
+package org.test;
+
+public class GitSample {
+
+	
+	public void git1() {
+		System.out.println("test1");
+
+	}
+	
+	public void git2() {
+		System.out.println("test2");
+
+	}
+	
+	public void git3() {
+		System.out.println("test3");
+
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		GitSample g = new GitSample();
+		
+		g.git1();
+		g.git2();
+		g.git3();
+		
+	}
+
+
+}
